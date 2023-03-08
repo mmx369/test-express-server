6df63ad572b615f7e61ef84524dddce122fdf459
mmx369 <maksim@klishevich.com>
2023-03-09 00:09:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 5000
+const cors = require('cors')
+const multer = require('multer')
+
+app.use(express.static('public'))
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage: storage })
+
+app.use(cors())
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/index.html'))
+// })
+
+app.get('/', (req, res) => {
+  res.send('Server is Ok')
+})
+
+app.post('/upload', upload.single('file'), function (req, res) {
+  res.json({
+    id: 'p1',
+    title: 'Молоко Простоквашино отборное 3.2%',
+    weight: 900,
+    measure: 'гр',
+    item: '12345',
+    kkal: 60,
+    protein: 8,
+    fats: 5,
+    carbohydrates: 3.3,
+    description: 'Волшебное молоко. Неповторимый вкус.',
+    price: 65,
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Test server running on port ${port}`)
+})
