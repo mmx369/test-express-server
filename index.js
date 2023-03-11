75cf5b91a37cbe1e5b3b2c1d64fe63c0fe5cf403
mmx369 <maksim@klishevich.com>
2023-03-11 22:35:38 +0300

    fix api

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const path = require('path')
 const app = express()
@@ -5,7 +6,37 @@ const port = 5000
 const cors = require('cors')
 const multer = require('multer')
 
+app.use(bodyParser.json())
 app.use(express.static('public'))
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    title: 'Молоко Простоквашино отборное 3.2%',
+    weight: 900,
+    measure: 'гр',
+    article: 12345,
+    kkal: 60,
+    protein: 8,
+    fats: 5,
+    carbohydrates: 3.3,
+    description: 'Волшебное молоко. Неповторимый вкус.',
+    price: 65,
+  },
+  {
+    id: 'p2',
+    title: 'Кефир Простоквашино отборное 3.2%',
+    weight: 1000,
+    measure: 'гр',
+    article: 12345,
+    kkal: 63,
+    protein: 8,
+    fats: 7,
+    carbohydrates: 3.6,
+    description: 'Волшебный кефир. Неповторимый вкус.',
+    price: 69,
+  },
+]
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -20,28 +51,20 @@ const upload = multer({ storage: storage })
 
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/index.html'))
-// })
-
 app.get('/', (req, res) => {
   res.send('Server is Ok')
 })
 
 app.post('/upload', upload.single('file'), function (req, res) {
-  res.json({
-    id: 'p1',
-    title: 'Молоко Простоквашино отборное 3.2%',
-    weight: 900,
-    measure: 'гр',
-    article: 12345,
-    kkal: 60,
-    protein: 8,
-    fats: 5,
-    carbohydrates: 3.3,
-    description: 'Волшебное молоко. Неповторимый вкус.',
-    price: 65,
-  })
+  setTimeout(() => {
+    res.json({ id: 'p1' })
+  }, 3000)
+})
+
+app.post('/find', (req, res) => {
+  const { id } = req.body.data
+  const product = DUMMY_PRODUCTS.filter((product) => product.id === id)
+  res.json(...product)
 })
 
 app.listen(port, () => {
